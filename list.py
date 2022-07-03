73678c15cc8874c3efd99698b073999209f24ae0
dltkdgns00 <s20604@naver.com>
2022-07-03 13:57:22 +0900

    python tutorial

---
+++ list.py
@@ -0,0 +1,13 @@
+students = ["이상훈","Lee","dltkdgns"]
+grades = [2,1,4]
+print("students[1]", students[1])
+print("len(students)", len(students))
+print("min(grandes)", min(grades))
+print("max(grandes)", max(grades))
+print("sum(grandes)", sum(grades))
+
+import statistics
+print("statistics.mean(grades)", statistics.mean(grades))
+
+import random
+print("random.choice(students)", random.choice(students))
---
+++ text.py
@@ -0,0 +1,11 @@
+print('Hello World')
+print("Hello World")
+print('''
+Hello
+World
+''')
+
+print("'1'+'1'",'1'+'1')
+print('Hello World'*1000)
+print("len('Hello World'*1000)",len('Hello World'*1000))
+print("'Hello World'.replace('World','Universe')"'Hello World'.replace('World','Universe'))
